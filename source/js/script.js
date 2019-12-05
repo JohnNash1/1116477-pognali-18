2d468e76b2d7cf0833f4f89feba8bb49fb6b5558
John Nash <mihailshtv@gmail.com>
2019-12-05 15:40:22 +0500

    сверстал меню для мобильных, оживил

---
+++ source/js/script.js
@@ -0,0 +1,21 @@
+var menuButton = document.querySelector(".main-navigation__toggle");
+var menu = document.querySelector(".main-navigation");
+var header = document.querySelector(".page-header");
+var logo = document.querySelector(".page-header__logo-image");
+
+menu.classList.remove("main-navigation--nojs");
+menu.classList.add("main-navigation--closed");
+menuButton.addEventListener("click", function () {
+  if (menu.classList.contains("main-navigation--closed")) {
+    menu.classList.remove("main-navigation--closed");
+    menu.classList.add("main-navigation--opened");
+    header.classList.add("page-header--menu-opened");
+    logo.setAttribute("src", "img/logo-mobile-blue@1x.png");
+  }
+  else {
+    menu.classList.remove("main-navigation--opened");
+    menu.classList.add("main-navigation--closed");
+    header.classList.remove("page-header--menu-opened");
+    logo.setAttribute("src", "img/logo-mobile-white@1x.png");
+  };
+});
