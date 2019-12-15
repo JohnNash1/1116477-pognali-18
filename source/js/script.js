639f1c452044a78a3b9cc7d0b341e65b346a8cbf
John Nash <mihailshtv@gmail.com>
2019-12-15 18:12:43 +0500

    планшетное меню

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var menuButton = document.querySelector(".main-navigation__toggle");
 var menu = document.querySelector(".main-navigation");
 var header = document.querySelector(".page-header");
 var logo = document.querySelector(".page-header__logo-image");
+var logoTablet = document.querySelector(".page-header__logo-tablet");
 
 menu.classList.remove("main-navigation--nojs");
 menuButton.addEventListener("click", function () {
@@ -10,11 +11,13 @@ menuButton.addEventListener("click", function () {
     menu.classList.add("main-navigation--opened");
     header.classList.add("page-header--menu-opened");
     logo.setAttribute("src", "img/logo-mobile-blue@1x.png");
+    logoTablet.setAttribute("srcset", "img/logo-tablet-blue@1x.png");
   }
   else {
     menu.classList.remove("main-navigation--opened");
     menu.classList.add("main-navigation--closed");
     header.classList.remove("page-header--menu-opened");
     logo.setAttribute("src", "img/logo-mobile-white@1x.png");
+    logoTablet.setAttribute("srcset", "img/logo-tablet-white@1x.png");
   };
 });
