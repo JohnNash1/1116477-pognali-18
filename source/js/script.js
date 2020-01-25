cd17d1b28b47c3a553f8d559927eb5b437c84514
John Nash <mihailshtv@gmail.com>
2020-01-25 06:28:55 +0500

    скролл-меню

--- source/js/script.js
+++ source/js/script.js
@@ -11,6 +11,7 @@ menuButton.addEventListener("click", function () {
     menu.classList.add("main-navigation--opened");
     header.classList.add("page-header--menu-opened");
     logo.setAttribute("src", "img/logo-mobile-blue@1x.png");
+    logo.setAttribute("srcset", "img/logo-mobile-blue@2x.png");
     logoTablet.setAttribute("srcset", "img/logo-tablet-blue@1x.png");
   }
   else {
@@ -18,6 +19,7 @@ menuButton.addEventListener("click", function () {
     menu.classList.add("main-navigation--closed");
     header.classList.remove("page-header--menu-opened");
     logo.setAttribute("src", "img/logo-mobile-white@1x.png");
+    logo.setAttribute("srcset", "img/logo-mobile-white@2x.png");
     logoTablet.setAttribute("srcset", "img/logo-tablet-white@1x.png");
   };
 });
@@ -26,62 +28,73 @@ var filterButton = document.querySelector(".catalog-filter__button");
 var filterMenu = document.querySelector(".catalog-filter__wrapper");
 var catalogFilter = document.querySelector(".catalog-filter");
 
-filterButton.addEventListener("click", function () {
-  if (filterMenu.classList.contains("catalog-filter__wrapper--closed")) {
-    filterMenu.classList.remove("catalog-filter__wrapper--closed");
-    filterMenu.classList.add("catalog-filter__wrapper--opened");
-    catalogFilter.classList.add("catalog-filter--opened");
-  }
-  else {
+if (filterButton) {
+  filterButton.addEventListener("click", function () {
+    if (filterMenu.classList.contains("catalog-filter__wrapper--closed")) {
+      filterMenu.classList.remove("catalog-filter__wrapper--closed");
+      filterMenu.classList.add("catalog-filter__wrapper--opened");
+      catalogFilter.classList.add("catalog-filter--opened");
+    }
+    else {
+      filterMenu.classList.remove("catalog-filter__wrapper--opened");
+      filterMenu.classList.add("catalog-filter__wrapper--closed");
+      catalogFilter.classList.remove("catalog-filter--opened");
+    };
+  });
+
+  var filterCloseButton = document.querySelector(".catalog-filter__close-button");
+
+  filterCloseButton.addEventListener("click", function () {
     filterMenu.classList.remove("catalog-filter__wrapper--opened");
     filterMenu.classList.add("catalog-filter__wrapper--closed");
     catalogFilter.classList.remove("catalog-filter--opened");
-  };
-});
+  });
+};
 
-var filterCloseButton = document.querySelector(".catalog-filter__close-button");
-
-filterCloseButton.addEventListener("click", function () {
-  filterMenu.classList.remove("catalog-filter__wrapper--opened");
-  filterMenu.classList.add("catalog-filter__wrapper--closed");
-  catalogFilter.classList.remove("catalog-filter--opened");
-});
 
 var hobbyButton = document.querySelector(".filter-group__hobby-button");
 var hobbyWrapper = document.querySelector(".filter-group__hobby-wrapper");
 
-hobbyButton.addEventListener("click", function () {
-  hobbyWrapper.classList.toggle("filter-group__wrapper--closed");
-  if (hobbyButton.classList.contains("filter-group__button--opened")) {
-    hobbyButton.classList.remove("filter-group__button--opened");
-  }
-  else {
-    hobbyButton.classList.add("filter-group__button--opened");
-  };
-});
+if(hobbyButton) {
+  hobbyButton.addEventListener("click", function () {
+    hobbyWrapper.classList.toggle("filter-group__wrapper--closed");
+    if (hobbyButton.classList.contains("filter-group__button--opened")) {
+      hobbyButton.classList.remove("filter-group__button--opened");
+    }
+    else {
+      hobbyButton.classList.add("filter-group__button--opened");
+    };
+  });
+};
+
 
 var musicButton = document.querySelector(".filter-group__music-button");
 var musicWrapper = document.querySelector(".filter-group__music-wrapper");
 
-musicButton.addEventListener("click", function () {
-  musicWrapper.classList.toggle("filter-group__wrapper--closed");
-  if (musicButton.classList.contains("filter-group__button--opened")) {
-    musicButton.classList.remove("filter-group__button--opened");
-  }
-  else {
-    musicButton.classList.add("filter-group__button--opened");
-  };
-});
+if (musicButton) {
+  musicButton.addEventListener("click", function () {
+    musicWrapper.classList.toggle("filter-group__wrapper--closed");
+    if (musicButton.classList.contains("filter-group__button--opened")) {
+      musicButton.classList.remove("filter-group__button--opened");
+    }
+    else {
+      musicButton.classList.add("filter-group__button--opened");
+    };
+  });
+};
+
 
 var foodButton = document.querySelector(".filter-group__food-button");
 var foodWrapper = document.querySelector(".filter-group__food-wrapper");
 
-foodButton.addEventListener("click", function () {
-  foodWrapper.classList.toggle("filter-group__wrapper--closed");
-  if (foodButton.classList.contains("filter-group__button--opened")) {
-    foodButton.classList.remove("filter-group__button--opened");
-  }
-  else {
-    foodButton.classList.add("filter-group__button--opened");
-  };
-});
+if (foodButton) {
+  foodButton.addEventListener("click", function () {
+    foodWrapper.classList.toggle("filter-group__wrapper--closed");
+    if (foodButton.classList.contains("filter-group__button--opened")) {
+      foodButton.classList.remove("filter-group__button--opened");
+    }
+    else {
+      foodButton.classList.add("filter-group__button--opened");
+    };
+  });
+};
